a525612eed3792d8c05ad1133156899b005d1881
Joedaimar R. Gozon <gozonjoedaimar@gmail.com>
2019-02-01 08:15:09 +0800

    Add ion auth test

--- application/modules/api/controllers/Swagger.php
+++ application/modules/api/controllers/Swagger.php
@@ -18,4 +18,18 @@ class Swagger extends MY_Controller {
 		header('Content-Type: application/json');		
 		echo $swagger;
 	}
+
+	/* Debugging */
+	public function ion_auth_test() 
+	{
+		$this->load->model('assessment_forms_model', 'assessment_forms');
+		$data = json_decode(file_get_contents('php://input'));
+
+		header('Content-Type: application/json');
+		echo json_encode([
+			"post"=>$data,
+			"confirm"=>$this->assessment_forms->admin_auth($data->username, $data->password),
+			"student_id_exists"=>$this->assessment_forms->student_id_exists($data->student_id)
+		]);
+	}
 }
